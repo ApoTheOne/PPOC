9e26f7b0fcd9b12b4183b13bf35ee65e91a69da1
ApoTheOne <anurag.saklani4@gmail.com>
2018-02-13 09:45:41 +0530

    Added structure for other endpoints

--- DotNetApi/Middlewares/RouteHandler.cs
+++ DotNetApi/Middlewares/RouteHandler.cs
@@ -29,20 +29,17 @@ namespace CorePOC.Middlewares
                             var regStream = new StreamReader(context.Request.Body);
                             var regBody = regStream.ReadToEnd();
                             var regResult = await _service.AddConsumerAsync(JsonConvert.DeserializeObject<Model.Consumer>(regBody));
-                            //await context.Response.WriteJson<string>(result);
+                            //await context.Response.WriteJson<string>(regResult);
                             context.Response.ContentType = "application/json";
                             await context.Response.WriteAsync(JsonConvert.SerializeObject(regResult));
                             break;
                         case "/login":
-
                             var loginStream = new StreamReader(context.Request.Body);
                             var body = loginStream.ReadToEnd();
                             var loginResult = await _service.GetConsumerDetailsAsync(JsonConvert.DeserializeObject<Model.Consumer>(body));
-                            //await context.Response.WriteJson<string>(result);
                             context.Response.ContentType = "application/json";
                             await context.Response.WriteAsync(JsonConvert.SerializeObject(loginResult));
                             break;
-
                     }
                     break;
                 case "GET":
@@ -64,11 +61,43 @@ namespace CorePOC.Middlewares
 
                         }
                     }
+                    else
+                    {
+                        var userId = context.Request.Query["userId"];
+                        Auth objauth = new Auth()
+                        {
+                            Username = context.Request.Headers["key"],
+                            Passowrd = context.Request.Headers["password"]
+                        };
+                        if (_service.IsAuthorized(objauth))
+                        {
+                            //var result = _service.GetCardDetailsAsync(userId);
+                            //var result=_service.GetConsumerDetails();
+                        }
+                        else
+                        {
+
+                        }
+                    }
                     break;
                 case "PUT":
                     if (path == "/cardDetails")
                     {
+                        Auth objauth = new Auth()
+                        {
+                            Username = context.Request.Headers["key"],
+                            Passowrd = context.Request.Headers["password"]
+                        };
+                        if (_service.IsAuthorized(objauth))
+                        {
+                            var regStream = new StreamReader(context.Request.Body);
+                            var regBody = regStream.ReadToEnd();
+                            //var regResult = await _service.AddConsumerAsync(JsonConvert.DeserializeObject<Model.Consumer>(regBody));
+                        }
+                        else
+                        {
 
+                        }
                     }
                     break;
             }
